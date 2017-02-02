bbccb1d523746fe4a85ede890530c1635401f1aa
Will J <willj.94@gmail.com>
2017-02-02 16:38:17 -0700

    LUA files linted and cleaned (closes #2)

--- data.lua
+++ data.lua
@@ -1,26 +1,26 @@
-require "util"
+require 'util'
 
 -- require tree items
-require("prototypes.item")
-require("prototypes.entity")
-require("prototypes.recipe")
-require("prototypes.technology")
+require('prototypes.item')
+require('prototypes.entity')
+require('prototypes.recipe')
+require('prototypes.technology')
 
-for name, tree in pairs (data.raw["tree"]) do
-	if not(string.find(name, "dead") or string.find(name, "dry") or string.find(name, "coral") or string.find(name, "stick") or name == "sapling") then
+for name, tree in pairs (data.raw['tree']) do
+	if not(string.find(name, 'dead') or string.find(name, 'dry') or string.find(name, 'coral') or string.find(name, 'stick') or name == 'sapling') then
 		tree.minable.result = nil
 		tree.minable.results = {
 			{
-				type = "item",
-				name = "raw-wood",
+				type = 'item',
+				name = 'raw-wood',
 				amount_min = 4,
 				amount_max = 4,
 				probability = 1
 			},
 			-- Construction robots won't be able to grab seeds, only manual mining can
 			{
-				type = "item",
-				name = "sapling",
+				type = 'item',
+				name = 'sapling',
 				amount_min = 2,
 				amount_max = 4,
 				probability = 1
--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,23 +1,12 @@
-data:extend(
-{
-	-- {
-	-- 	type = "item",
-	-- 	name = "seed",
-	-- 	icon = "__SimpleSaplings__/graphics/icons/seed.png",
-	-- 	flags = { "goes-to-main-inventory" },
-	-- 	fuel_value = "1MJ",
-	-- 	subgroup = "raw-material",
-	-- 	order = "a[seed]",
-	-- 	stack_size = 100
-	-- },
-	{
-		type = "item",
-		name = "sapling",
-		icon = "__SimpleSaplings__/graphics/icons/seed.png",
-		flags = { "goes-to-quickbar" },
-		subgroup = "raw-material",
-		order = "a[sapling]",
-		place_result = "sapling",
-		stack_size = 10
-	}
+data:extend({
+  {
+    type = 'item',
+    name = 'sapling',
+    icon = '__SimpleSaplings__/graphics/icons/seed.png',
+    flags = { 'goes-to-quickbar' },
+    subgroup = 'raw-material',
+    order = 'a[sapling]',
+    place_result = 'sapling',
+    stack_size = 10
+  }
 })
