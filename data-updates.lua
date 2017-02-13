9d2f07a2df8fadaaadf8760e68703e6db2abf7af
Nexela <nexela@gmail.com>
2017-02-13 05:28:30 -0500

    Add seedlings into trees in data-updates.lua

---
+++ data-updates.lua
@@ -0,0 +1,22 @@
+for name, tree in pairs (data.raw['tree']) do
+	if not(string.find(name, 'dead') or string.find(name, 'dry') or string.find(name, 'coral') or string.find(name, 'stick') or name == 'sapling') then
+		tree.minable.result = nil
+		tree.minable.results = {
+			{
+				type = 'item',
+				name = 'raw-wood',
+				amount_min = 4,
+				amount_max = 4,
+				probability = 1
+			},
+			-- Construction robots won't be able to grab seeds, only manual mining can
+			{
+				type = 'item',
+				name = 'sapling',
+				amount_min = 2,
+				amount_max = 4,
+				probability = 1
+			}
+		}
+	end
+end
